612886bacd49909684c6b995d0bf70c10c14dcb1
mac033 <mac033@mac033tekiiMac.local>
2017-11-16 12:21:24 +0800

    finished mult.asm

--- exercise/Mult.asm
+++ exercise/Mult.asm
@@ -4,9 +4,7 @@
 // File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-
-// Put your code here.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
     @2
     M=0     // R2 = 0
